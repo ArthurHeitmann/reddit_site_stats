23f184d6cb4b6d5f4fa5891136a67549c376d27a
ArthurHeitmann <37270165+ArthurHeitmann@users.noreply.github.com>
2023-06-09 13:20:05 +0200

    Added script that auto restarts node script and sends notifications if it crashes

---
+++ runAndWatch.py
@@ -0,0 +1,30 @@
+import subprocess
+import pushbullet
+from pushbullet import Pushbullet
+from dotenv import load_dotenv
+import os
+import traceback
+import time
+
+load_dotenv()
+
+apiKey = os.getenv("pushbulletApiKey")
+if apiKey is None:
+    print("pushbulletApiKey not set in .env file")
+    exit()
+command = ["node", "src/app.js"]
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+try:
+    while True:
+        try:
+            process = subprocess.check_call(command)
+        except subprocess.CalledProcessError:
+            print("Script crashed")
+            traceback.print_exc()
+            pb = Pushbullet(apiKey)
+            push = pb.push_note("Script Crash Alert", "Your script has crashed.")
+            print("Waiting 30 seconds before restarting")
+            time.sleep(30)
+except KeyboardInterrupt:
+    print("Script stopped by user")
